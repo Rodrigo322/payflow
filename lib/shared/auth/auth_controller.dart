fa1848f73050a08997d71c07e4e4c8af2f1b1849
rodrigo322 <rodrigollrdglucas@gmail.com>
2023-03-17 15:43:18 -0300

    adicionando carregamento da splash

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context , var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
       _user = user;
-      _isAuthenticated = true;
       //navega para tela home_page
       Navigator.pushReplacementNamed(context, "/home");
     } else {
       //navega para tela login_page
-      _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(Duration(seconds: 2));
+    if(instance.containsKey("user")) {
+      final user = instance.get("user");
+      setUser(context, UserModel.fromJson(user));
+      return;
+    } else {
+      setUser(context,null);
     }
   }
 }
---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+class UserModel {
+  final String name;
+  final String? photoURL;
+
+  UserModel({required this.name, this.photoURL});
+
+  Map<String, dynamic> toMap() => {
+        "name": name,
+        "photoURL": photoURL,
+      };
+  factory UserModel.fromJson(json) => UserModel.fromMap(jsonDecode(json));
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(name: map["name"], photoURL: map["photoURL"]);
+  }
+
+  String toJson() => jsonEncode(toMap);
+}
